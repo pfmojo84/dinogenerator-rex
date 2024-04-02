e3dbdd13131e7213bc90006e8f4a036c4e6fe727
jdporr <jdporrazzo@gmail.com>
2024-04-02 12:10:20 -0400

    update script.js to populate random dinoName and dinoImage on click event

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
 document.querySelector('#btnLoad').addEventListener('click', () => {
+    if(document.querySelector('#dinoName') !== null) {
+        document.querySelector('#dinoName').remove();
+    }
+    if (document.querySelector('#dinoImage') !== null) {
+        document.querySelector('#dinoImage').remove();
+    }
+
+
     getDinoName();
-    getDinoImage();
 })
 
 
@@ -11,13 +18,28 @@ async function getDinoName(){
     const data = await response.json();
     let dinoName = data[0].join(' ');
     console.log(dinoName);
+    
+
+    let dinoNameDiv = document.createElement('div');
+    dinoNameDiv.id = 'dinoName';
+    dinoNameDiv.textContent = dinoName;
+    document.querySelector('#dinoWrapper').appendChild(dinoNameDiv);
+    getDinoImage();
 };
 
 async function getDinoImage(){
     const response = await fetch('/dinoimage');
     const data = await response.json();
-    let dinoImage = data;
+    let dinoImage = data.response.suggestions[Math.floor(Math.random() * data.response.suggestions.length)];
+    let dinoImageURL = dinoImage.thumbnail;
+    let dinoAlt = dinoImage.text;
     console.log(dinoImage);
+
+    let img = document.createElement('img');
+    img.id = 'dinoImage';
+    img.src = dinoImageURL;
+    img.alt = dinoAlt;
+    document.querySelector('#dinoWrapper').appendChild(img);
+
 };
 
-getDinoName();
